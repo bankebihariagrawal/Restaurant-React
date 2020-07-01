506c03cf58f3bd069916618d354a9810a7fecd8d
banke bihari agrawal <bankebihariagrawal58@gmail.com>
2020-07-01 15:28:35 +0530

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from './ContactComponent';
 import {COMMENTS} from '../shared/comments';
 import {PROMOTIONS} from '../shared/promotions';
 import {LEADERS} from '../shared/leaders';
+import About from './AboutComponent';
 class Main extends Component {
   constructor(props){
     super(props);
@@ -51,6 +52,7 @@ class Main extends Component {
          <Route path="/home" component={HomePage} />
          <Route exact path = '/menu' component={() => <Menu dishes = {this.state.dishes}/>} />
          <Route path="/menu/:dishId" component={DishWithId} />
+         <Route exact path="/aboutus" component={() => <About leaders= {this.state.leaders}  />} />
          <Route exact path='/contactus' component={Contact} />
          <Redirect to="/home" />
        </Switch>
